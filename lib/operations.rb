41fc1f5c7f569ede554344357282e0d59b26f684
Samuel Cornet <samuel@noukod.com>
2018-09-18 14:07:59 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
 
+speed = 40
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
 end
-
-
+end
 
 def not_safe?(speed)
-	
+ speed >= 40? ? true : false
 end
-	
-
-
